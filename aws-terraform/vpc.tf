bf16d6f1372b53ee1034788c38811687b776c3b3
igor-primo <igorprimo62@gmail.com>
2024-08-29 18:01:39 -0300

    feat: add vpc resource

---
+++ aws-terraform/vpc.tf
@@ -0,0 +1,9 @@
+# Create a VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "aws-terraform-vpc"
+  }
+}
