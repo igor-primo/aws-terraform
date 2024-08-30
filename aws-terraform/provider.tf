d1efb5ad676043b0fbf866b808aa51e4f677c57f
igor-primo <igorprimo62@gmail.com>
2024-08-30 12:42:01 -0300

    feat: add s3 backend to store terraform.tfstate

--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.64.0"
     }
   }
+  backend "s3" {
+    bucket = "devopstraining-bucket-igor"
+    # Keys can be used to store different states
+    # each of each environment of a project
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
