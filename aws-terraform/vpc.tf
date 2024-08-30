4ef4df49fcdb11bce4fa5321cf05b344a678e43c
igor-primo <igorprimo62@gmail.com>
2024-08-30 13:47:04 -0300

    refactor: use dynamic variables to cidr block

--- aws-terraform/vpc.tf
+++ aws-terraform/vpc.tf
@@ -1,6 +1,6 @@
 # Create a VPC
 resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
