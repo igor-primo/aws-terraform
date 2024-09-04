1f99577023a9df3f7e377d3c04ab55a5ae4826a6
igor-primo <igorprimo62@gmail.com>
2024-09-04 12:25:38 -0300

    fix: make cluster export oidc to be used by the load balancer

--- aws-terraform/modules/cluster/iam.tf
+++ aws-terraform/modules/cluster/iam.tf
@@ -1,8 +1,6 @@
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.project_name}-cluster-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
