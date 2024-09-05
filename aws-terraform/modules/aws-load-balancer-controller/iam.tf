563c3b8df33b5cbbf92ec162bcb7ccbc52c2542d
igor-primo <igorprimo62@gmail.com>
2024-09-05 12:51:10 -0300

    fix: a typo

--- aws-terraform/modules/aws-load-balancer-controller/iam.tf
+++ aws-terraform/modules/aws-load-balancer-controller/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_controller_role" {
-  name = "${var.project_name}-aws-load-balancer-contoller"
+  name = "${var.project_name}-aws-load-balancer-controller"
 
   assume_role_policy = <<EOF
 {
