13833ac3d1752f6966d1dc03a26e7799c7d59730
igor-primo <igorprimo62@gmail.com>
2024-09-11 12:22:55 -0300

    refactor: modularize super module

--- aws-terraform/modules.tf
+++ aws-terraform/modules.tf
@@ -2,13 +2,13 @@ module "eks_network" {
   cidr_block   = var.cidr_block
   project_name = var.project_name
   source       = "./modules/network"
-  tags         = local.tags
+  tags         = var.tags
 }
 
 module "eks_cluster" {
   source           = "./modules/cluster"
   project_name     = var.project_name
-  tags             = local.tags
+  tags             = var.tags
   public_subnet_1a = module.eks_network.subnet_pub_1a
   public_subnet_1b = module.eks_network.subnet_pub_1b
 }
@@ -19,13 +19,13 @@ module "eks_managed_node_group" {
   subnet_private_1a = module.eks_network.subnet_priv_1a
   subnet_private_1b = module.eks_network.subnet_priv_1b
   project_name      = var.project_name
-  tags              = local.tags
+  tags              = var.tags
 }
 
 module "eks_aws_load_balancer_controller" {
   source       = "./modules/aws-load-balancer-controller"
   project_name = var.project_name
-  tags         = local.tags
+  tags         = var.tags
   oidc         = module.eks_cluster.oidc
   cluster_name = module.eks_cluster.cluster_name
 }
--- aws-terraform/provider.tf
+++ aws-terraform/provider.tf
@@ -13,17 +13,10 @@ terraform {
       version = "2.15.0"
     }
   }
-  backend "s3" {
-    bucket = "devopstraining-bucket-igor"
-    # Keys can be used to store different states
-    # each of each environment of a project
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 provider "kubernetes" {
